1a49bc65db964b8d9b47ffa5781a40f6abebf30e
maftuhin <maftuhin@mynameisdav.com>
2024-05-09 20:12:20 +0700

    db access

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const { Sequelize, QueryTypes } = require('sequelize');
 const app = express()
 const port = 8000
 
-const sequelize = new Sequelize('mysql://root:118806@localhost:3306/family') // Example for postgres
+// const sequelize = new Sequelize('mysql://root:118806@localhost:3306/family')
+const sequelize = new Sequelize(
+    'mysql://mafb1319_main:8Belas0694s@localhost:3306/mafb1319_family'
+)
 
 app.get('/', async (req, res) => {
     const data = await sequelize.query("SELECT * FROM people", {
