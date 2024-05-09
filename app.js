aa96021d25e69ba97ac90c2686d47a5f7df05e75
maftuhin <maftuhin@mynameisdav.com>
2024-05-09 20:10:43 +0700

    add sequelize

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
-import mysql from "mysql2/promise"
-import express from "express"
+const express = require('express')
+const { Sequelize, QueryTypes } = require('sequelize');
 const app = express()
+const port = 8000
 
-// Create the connection to database
-// const connection = await mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'family',
-//     password: "118806"
-// });
+const sequelize = new Sequelize('mysql://root:118806@localhost:3306/family') // Example for postgres
 
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'mafb1319_main',
-    database: 'mafb1319_family',
-    password: "8Belas0694s"
-});
-
-app.get("/", (req, res) => {
-    res.send("Index")
-})
-
-app.get("/user", async (req, res) => {
-    const q = req.query["q"] || ""
-    const sql = "SELECT id, uid, name, address FROM `people` WHERE `name` LIKE '%" + q + "%'"
-    const [rows, field] = await connection.query(sql)
-    res.json({
-        records: rows
+app.get('/', async (req, res) => {
+    const data = await sequelize.query("SELECT * FROM people", {
+        type: QueryTypes.SELECT
     })
+    res.send(data)
 })
 
-app.get("/family/:uid", async (req, res) => {
-    const person = await connection.query("select * from people LIMIT 1");
-    res.send(person)
-})
-app.listen()
+app.listen(port)
